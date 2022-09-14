8dab65e256f17932bebec1510f6f355e1bcf8b24
Amadey Vorontsov <vorontsov.aa@phystech.edu>
2022-09-14 22:19:00 +0300

    alpha version

--- test_sfml.cpp
+++ test_sfml.cpp
@@ -12,8 +12,12 @@ int main()
     SystemCoord sys(O, e1, e2);
     Vector vec(120, 0, sys);
     vec.setColor(sf::Color::Red);
+
+    Vector vec2(800, 0, sys);
+    vec2.setAppPoint(400, 0, sys);
+    std::cout << vec2 << '\n';
     
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SOSAT!");
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "SOSAT!");
     sf::CircleShape shape(100.f);
     shape.setOrigin(sf::Vector2f(-100, -100));
     shape.setFillColor(sf::Color::Green);
@@ -24,18 +28,29 @@ int main()
         sf::Event event;
         while (window.pollEvent(event))
         {
+            sf::Vector2i pixelPos = sf::Mouse::getPosition(window);
+            
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.key.code == sf::Mouse::Left)
+                {
+                    vec2.SetX(pixelPos.x);
+                    vec2.SetY(pixelPos.y);
+                }
+            }
         }
 
         window.clear();
         
-        vec.SetX(100 * cos(t * 0.0003), sys);
-        vec.SetY(100 * sin(t * 0.0003), sys);
+        vec.SetX(100 * cos(-t * 0.0003), sys);
+        vec.SetY(100 * sin(-t * 0.0003), sys);
         t++;
 
-        window.draw(shape);
+        //window.draw(shape);
         vec.draw(window);
+        vec2.draw(window);
                 
         window.display();
     }
--- vector.h
+++ vector.h
@@ -8,8 +8,33 @@ struct Point
 {
     double cx;
     double cy;
+    double clen = NAN;
 
     Point(double x = 0, double y = 0) : cx(x), cy(y) {}
+    Point operator+(const Point& right) const
+    {
+        return Point(cx + right.cx, cy + right.cy);
+    }
+    Point operator/(double number)
+    {
+        return Point(cx / number, cy / number);
+    }
+    Point operator*(double number)
+    {
+        return Point(cx * number, cy * number);
+    }
+    double length() 
+    { 
+        if (std::isnan(clen))
+        {
+            clen = std::sqrt(cx * cx + cy * cy);
+        }
+        return clen;
+    }
+    Point normalize()
+    {
+        return Point(cx / length(), cy / length());
+    }
 };
 
 
@@ -20,8 +45,8 @@ private:
     double clen;
 
     Point capp_point = Point(0 , 0);
-
     sf::VertexArray cline = sf::VertexArray(sf::Lines, 2);
+    sf::VertexArray carrow = sf::VertexArray(sf::LineStrip, 3);
     sf::Color ccolor = sf::Color::White;
 
 public:
@@ -40,15 +65,9 @@ public:
         return clen;
     }
 
-    void setAppPoint(const Point& app_point)
-    {
-        capp_point = app_point;
-    }
-
-    void setAppPoint(int x, int y)
-    {
-        capp_point = Point(x, y);
-    }
+    void setAppPoint(const Point& app_point, const SystemCoord& sys_coord);
+    void setAppPoint(int x, int y, const SystemCoord& sys_coord);
+    
 
     void setColor(const sf::Color& color)
     {
@@ -57,14 +76,43 @@ public:
 
     void SetX(double x, const SystemCoord& sys_coord);
     void SetY(double y, const SystemCoord& sys_coord);
+    void SetX(double x);
+    void SetY(double y);
 
     void draw(sf::RenderWindow& window)
     {
-        cline[0].position = sf::Vector2f(capp_point.cx + 1, capp_point.cy);
-        cline[1].position = sf::Vector2f(capp_point.cx + cx + 1, capp_point.cy + cy);
+        const double cos30 = std::cos(7 * M_PI / 6); 
+        const double sin30 = std::sin(7 * M_PI / 6); 
+
+        auto left_point = Point(cos30 * (cx - capp_point.cx) + sin30 * (cy - capp_point.cy),
+                               -sin30 * (cx - capp_point.cx) + cos30 * (cy - capp_point.cy));
+        left_point = left_point.normalize() * 10;
+
+        auto right_point = Point(cos30 * (cx - capp_point.cx) - sin30 * (cy - capp_point.cy),
+                                 sin30 * (cx - capp_point.cx) + cos30 * (cy - capp_point.cy));
+
+        right_point = right_point.normalize() * 10;
+
+        auto left_point_vec  = sf::Vector2f(left_point.cx + cx, left_point.cy + cy);
+        auto right_point_vec = sf::Vector2f(right_point.cx + cx, right_point.cy + cy);
+        
+        cline[0].position = sf::Vector2f(capp_point.cx, capp_point.cy);
+        cline[1].position = sf::Vector2f(cx, cy);
+
+        carrow[0].position = left_point_vec;
+        carrow[1].position = cline[1].position;
+        carrow[2].position = right_point_vec;
+
+        
         cline[0].color    = ccolor;
         cline[1].color    = ccolor;
+        carrow[0].color   = sf::Color::Green;
+        carrow[1].color   = sf::Color::Blue;
+        carrow[2].color   = sf::Color::Yellow;
+
+
         window.draw(cline);
+        window.draw(carrow);
     }
 
     Vector operator+(const Vector& rvec) const
@@ -124,19 +172,42 @@ struct SystemCoord
 Vector::Vector(double x, double y, const SystemCoord sys_coord)
 {
     capp_point = sys_coord.cstart_point;
-    cx = sys_coord.ce1.cx * x + sys_coord.ce2.cx * y;
-    cy = sys_coord.ce1.cy * x + sys_coord.ce2.cy * y;
+    cx = sys_coord.ce1.cx * x + sys_coord.ce2.cx * y + sys_coord.cstart_point.cx;
+    cy = sys_coord.ce1.cy * x + sys_coord.ce2.cy * y + sys_coord.cstart_point.cy;
     clen = NAN;
 }
 
 void Vector::SetX(double x, const SystemCoord& sys_coord)
 {
-    cx = sys_coord.ce1.cx * x + sys_coord.ce2.cx * cy;
+    cx = sys_coord.ce1.cx * x + sys_coord.ce2.cx * cy + sys_coord.cstart_point.cx;
     clen = NAN;
 }
 
 void Vector::SetY(double y, const SystemCoord& sys_coord)
 {
-    cy = sys_coord.ce1.cy * cx + sys_coord.ce2.cy * y;
+    cy = sys_coord.ce1.cy * cx + sys_coord.ce2.cy * y + sys_coord.cstart_point.cy;
     clen = NAN;
+}
+
+
+void Vector::SetX(double x)
+{
+    cx = x;
+    clen = NAN;
+}
+
+void Vector::SetY(double y)
+{
+    cy = y;
+    clen = NAN;
+}
+
+void Vector::setAppPoint(const Point& app_point, const SystemCoord& sys_coord)
+{
+    capp_point = app_point + sys_coord.cstart_point;
+}
+
+void Vector::setAppPoint(int x, int y, const SystemCoord& sys_coord)
+{
+    capp_point = Point(x, y) + sys_coord.cstart_point;
 }
